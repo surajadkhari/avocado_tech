5c68c0aff600fbf2fad0493e2c9a81d6d4e30197
surajadkhari <surajadkhari@gmail.com>
2022-04-16 11:41:02 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/avacado_tech/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.avacado_tech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../screens/homepage/homepage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
