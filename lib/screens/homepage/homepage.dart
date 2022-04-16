f82a1a730f783f5213205d017ca641b0b23c5b9e
surajadkhari <surajadkhari@gmail.com>
2022-04-16 12:13:55 +0545

    Complete appbar

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -1,12 +1,24 @@
+import 'package:avacado_tech/utils/constant/app_constant.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({ Key? key }) : super(key: key);
+  const Homepage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: AppConstant.primaryColor,
+        leading: const Center(child: Text("Doctor")),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.filter_list_outlined,
+              ))
+        ],
+      ),
     );
   }
 }
---
+++ lib/utils/constant/app_constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppConstant{
+  AppConstant._();
+  static const primaryColor = Color(0xff24627F);
+}
